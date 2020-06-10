8766ae376ecaafa7e2fbafa40f6610100933462e
In  Seo <inseo.kr@gmail.com>
2020-06-09 18:29:36 -0700

    enabling environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,70 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const outputDirectory = 'dist';
 
-module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'],
-  output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = () => {
+
+  const env = dotenv.config().parsed;
+
+  // reduce it to a nice object, the same as before
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+
+    return prev;
+
+  }, {});
+  
+  return {
+    entry: ['babel-polyfill', './src/client/index.js'],
+    output: {
+      path: path.join(__dirname, outputDirectory),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg)$/,
+          loader: 'url-loader?limit=100000'
+        },
+        {
+          test: /\.(mp3)$/,
+          loader: 'file-loader'
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg)$/,
-        loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /\.(mp3)$/,
-        loader: 'file-loader'
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    devServer: {
+      port: 3000,
+      open: true,
+      proxy: {
+        '/api': 'http://localhost:8080'
       }
+    },
+    plugins: [
+      new CleanWebpackPlugin([outputDirectory]),
+      new webpack.DefinePlugin(envKeys),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: './public/favicon.ico'
+      })
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico'
-    })
-  ]
+  };
+
 };
