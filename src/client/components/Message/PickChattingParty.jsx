6a138191f0a7393fe860dd15213b4c186621a184
In  Seo <inseo.kr@gmail.com>
2020-06-18 20:31:50 -0700

    add missing file

---
+++ src/client/components/Message/PickChattingParty.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../../app.css';
+import './MessageStyle.css';
+
+function PickChattingParty(props) {
+
+  return (
+      <div>
+        Pick a friend!!
+      </div>
+  );
+}
+export default PickChattingParty;
