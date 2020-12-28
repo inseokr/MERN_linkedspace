ca445e2045b594f5e4f1e7f1925b6584eb40d3f1
Justin Seo <carrier_mania@yahoo.com>
2020-12-28 15:53:42 -0800

    issue-181: map changes

--- src/client/globalConstants.js
+++ src/client/globalConstants.js
@@ -1,5 +1,5 @@
-export const STYLESHEET_URL = (process.env.REACT_APP_NODE_ENV === 'development') ? 'http://10.0.0.107:5000' : process.env.REACT_APP_EXPRESS_SERVER_URL;
-export const FILE_SERVER_URL = (process.env.REACT_APP_NODE_ENV === 'development') ? 'http://10.0.0.107:5000/LS_API' : process.env.REACT_APP_FILE_SERVER_URL;
-export const API_SERVER_URL = (process.env.REACT_APP_NODE_ENV === 'development') ? 'http://10.0.0.107:5000/LS_API' : process.env.REACT_APP_EXPRESS_SERVER_URL;
+export const STYLESHEET_URL = (process.env.REACT_APP_NODE_ENV === 'development') ? 'http://localhost:5000' : process.env.REACT_APP_EXPRESS_SERVER_URL;
+export const FILE_SERVER_URL = (process.env.REACT_APP_NODE_ENV === 'development') ? 'http://localhost:5000/LS_API' : process.env.REACT_APP_FILE_SERVER_URL;
+export const API_SERVER_URL = (process.env.REACT_APP_NODE_ENV === 'development') ? 'http://localhost:5000/LS_API' : process.env.REACT_APP_EXPRESS_SERVER_URL;
 
 console.log(`Global Constants: env = ${JSON.stringify(process.env)}`);
