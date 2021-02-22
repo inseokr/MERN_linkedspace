a1e66795bdd85549b0acd4d45cb2b6abdd5092eb
In Seo <inseo.kr@gmail.com>
2021-02-21 19:23:29 -0800

    #fixing server crash, fetching it when user is not logged in?

--- src/server/routes/listing/landlord/index.js
+++ src/server/routes/listing/landlord/index.js
@@ -453,12 +453,16 @@ module.exports = function (app) {
       };
       res.json(listing_info);
 
-      userDbHandler.findUserById(req.user._id).then(async (foundUser) => {
-        if (!foundListing.requester.equals(foundUser._id)) {
-          userDbHandler.readListingFromFriends(foundUser, 'landlord', req.params.list_id);
-          foundUser.save();
-        }
-      });
+      try {
+        userDbHandler.findUserById(req.user._id).then(async (foundUser) => {
+          if (!foundListing.requester.equals(foundUser._id)) {
+            userDbHandler.readListingFromFriends(foundUser, 'landlord', req.params.list_id);
+            foundUser.save();
+          }
+        });
+      } catch (error) {
+        console.warn(error);
+      }
     });
   });
 
