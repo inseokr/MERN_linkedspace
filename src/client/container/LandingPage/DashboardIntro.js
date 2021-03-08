03003479e81120328a99b3078f43ebbc186cd13f
In Seo <inseo.kr@gmail.com>
2021-03-07 16:13:32 -0800

    #572

---
+++ src/client/container/LandingPage/DashboardIntro.js
@@ -0,0 +1,74 @@
+/* eslint-disable */
+import React from 'react';
+import { FILE_SERVER_URL } from '../../globalConstants';
+
+function DashboardIntro(props) {
+
+    let IntroMainStyle = {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr 2fr',
+        gridTemplateAreas: `"im im im ti"`,
+        marginTop: '50px'
+    };
+
+    let IntroStyle = {
+        minWidth: '50vh',
+        gridArea: 'ti',
+        marginLeft: '20px'
+    };
+
+    let ImgStyle = {
+        maxWidth: '110vh',
+        marginTop: '50px',
+        marginLeft: '100px'
+
+    };
+
+    let urlImg = "/public/user_resources/pictures/LinkedSpaces_Dashboard.jpg";
+
+    function handleSignUp() {
+        console.warn(`handleSignUp`);
+        props.handleSignupClick();
+    }
+
+    return (
+        <div style={IntroMainStyle}> 
+            <div style={{gridArea: 'im'}}> 
+                <img src={`${FILE_SERVER_URL}${urlImg}`} style={ImgStyle}></img>
+            </div>
+            <div style={IntroStyle}>
+                <section className="_lsTitleLarge" style={{textAlign: 'left'}}> Amazing Collaboration Tool  </section>
+                <section className="_lsMediaMedium" style={{marginTop: '25px'}}> 
+                    Sick and tired of dealing with information spread over all different sites and services when you're looking for room/houses or tenants?
+                    <br></br>
+                    <br></br>
+                    LinkedSpaces offers an efficent tool to keep all the information in one place and communication among friends has never been so easy and effective with this tool.
+                    <br></br>
+                    <br></br>
+                    List of key features provided.
+                    <br></br>
+                    <br></br>
+                    <ul>
+                        <li> Integtrated Chatting Service                        
+                        </li>
+                        <li> Integrated Map Service
+                        </li>
+                        <li> Liking/Rating Service
+                        </li>
+                        <li> Sharing postings from other sources like AirBnB, Craigslist, etc
+                        </li>
+                        <li> Realtime dashboard shared with friends
+                        </li>
+                    </ul>
+
+                </section>
+                <br></br>
+                <section className="_lsClickForDetails" onClick={handleSignUp} style={{cursor: 'pointer'}}>
+                    {`Sign up to enjoy this >`}
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardIntro;
