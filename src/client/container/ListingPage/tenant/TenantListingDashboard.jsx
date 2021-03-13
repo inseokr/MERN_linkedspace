361c999ade6393b7e7c57d375f2efe17dce66401
Justin Seo <carrier_mania@yahoo.com>
2021-03-12 23:58:33 -0800

    issue-486: initial refactoring

--- src/client/container/ListingPage/tenant/TenantListingDashboard.jsx
+++ src/client/container/ListingPage/tenant/TenantListingDashboard.jsx
@@ -31,22 +31,24 @@ import { GlobalContext } from '../../../contexts/GlobalContext';
 import { FILE_SERVER_URL } from '../../../globalConstants';
 import { preprocessUrlRequest } from '../../../utils/route_helper';
 
-// import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Circle } from 'react-leaflet'
 
 function TenantListingDashBoard(props) {
   const {loginClickHandler, hideLoginModal} = props;
 
   const {friendsMap, isUserLoggedIn, setRedirectUrlAfterLogin, currentUser} = useContext(GlobalContext);
-  const {doNotDisturbMode,
-         childIndex,
-         setChildIndex,
-         setDoNotDisturbMode,
-         broadcastDashboardMode,
-         toggleCollapse,
-         collapse,
-         setChattingContextType,
-         chattingContextType } = useContext(MessageContext);
+
+  const {
+    doNotDisturbMode,
+    childIndex,
+    setChildIndex,
+    setDoNotDisturbMode,
+    broadcastDashboardMode,
+    toggleCollapse,
+    collapse,
+    setChattingContextType,
+    chattingContextType
+  } = useContext(MessageContext);
 
   const { currentListing, currentChildIndex, setCurrentChildIndex, getChildListingUrl, fetchCurrentListing, mapParams, filterParams, setFilterParams, markerParams, setMarkerParams } = useContext(CurrentListingContext);
 
@@ -65,10 +67,9 @@ function TenantListingDashBoard(props) {
     if (node !== null && map !== null) {
       let bounds = node.getBounds();
       if (Object.keys(bounds).length > 0) {
-        //fitBounds will happen only if parent listing is selected.
-        if(currentChildIndex===-1) {
+        if (currentChildIndex === -1) { //fitBounds will happen only if parent listing is selected.
           map.fitBounds(bounds);
-          if(currBounds===null) {
+          if (currBounds === null) {
             setCurrBounds(bounds);
           }
         }
@@ -85,14 +86,10 @@ function TenantListingDashBoard(props) {
   };
 
   const handleClickDoNotDisturbMode = () => {
-
-    if(currentListing.requester.username === currentUser.username )
-    {
+    if (currentListing.requester.username === currentUser.username) {
       broadcastDashboardMode((doNotDisturbMode===false)? "locked": "normal");
     }
-
     setDoNotDisturbMode(!doNotDisturbMode);
-
   };
 
   const onMarkerClick = (e, selectedMarkerID) => {
@@ -110,19 +107,16 @@ function TenantListingDashBoard(props) {
     //console.warn(`refresh=${refresh}`);
     if (refresh) {
       const markers = []; // New list of markers.
-      const { coordinates, _id } = currentListing;
+      const { coordinates, _id, child_listings } = currentListing;
       if (validCoordinates(coordinates)) {
-        //console.warn(`createMarker`);
         const imgSource = currentListing.profile_pictures.length === 0 ? FILE_SERVER_URL+'/public/user_resources/pictures/5cac12212db2bf74d8a7b3c2_1.jpg' : FILE_SERVER_URL+currentListing.profile_pictures[0].path;
         const icon = createMarker({type: "image", data: imgSource}, (_id === selectedMarkerID));
-        markers.push({ position: coordinates, icon: icon, markerID: _id });
+        markers.push({ position: coordinates, icon: icon, markerID: _id }); // Add parent listing marker.
       }
 
-      // Get Location of any child listings if they exist.
-      if (currentListing.child_listings) { // Proceed if child listings exist.
-        const childListings = currentListing.child_listings;
-        if (childListings.length > 0) {
-          childListings.map((listing) => {
+      if (child_listings) { // Add child listings markers if they exist.
+        if (child_listings.length > 0) {
+          child_listings.map((listing) => {
             if(listing===null || listing.listing_id===null) return;
 
             const rentalPrice = (listing.listing_id.listingType === "landlord") ? Number(listing.listing_id.rental_terms.asking_price) : listing.listing_id.rentalPrice;
@@ -144,9 +138,9 @@ function TenantListingDashBoard(props) {
                   } else {
                     imgSource = FILE_SERVER_URL+listing.listing_id.coverPhoto.path;
                   }
-                  const _price = (listing.listing_id.listingType === "landlord")? 
-                                  listing.listing_id.rental_terms.asking_price: 
-                                  listing.listing_id.rentalPrice;
+                  const _price = (listing.listing_id.listingType === "landlord")?
+                    listing.listing_id.rental_terms.asking_price:
+                    listing.listing_id.rentalPrice;
                   const icon = createMarker({type: "price", data: _price}, (_id === selectedMarkerID));
                   markers.push({ position: coordinates, icon: icon, markerID: _id });
                 }
@@ -217,7 +211,7 @@ function TenantListingDashBoard(props) {
 
   useEffect(() => {
     //console.warn(`length of markers=${markers.length}`);
-  }, [markers])
+  }, [markers]);
 
   useEffect(() => {
     //console.warn(`markerParams`);
@@ -246,13 +240,13 @@ function TenantListingDashBoard(props) {
   }, [currentListing]);
 
   function controlMessageWindow() {
-    if( collapse == "true") {
+    if( collapse === "true") {
       // let's move the marker if possible
       // <note> not sure but the map's not moving as calculated.
       // I had to multiply 10 or any bigger value
-      let _coordinate = 
-      (currentChildIndex===-1) ? 
-        currentListing.coordinates : 
+      let _coordinate =
+        (currentChildIndex===-1) ?
+          currentListing.coordinates :
           (currentListing.child_listings[currentChildIndex].listing_id.listingType==="landlord") ?
             currentListing.child_listings[currentChildIndex].listing_id.rental_property_information.coordinates:
             currentListing.child_listings[currentChildIndex].listing_id.coordinates;
@@ -283,18 +277,18 @@ function TenantListingDashBoard(props) {
   let chatContainerStyle = { position: 'absolute', top: '0', right: '20%'};
 
   return (
-    (isUserLoggedIn()===false)? 
+    (isUserLoggedIn()===false)?
       <div>
-          <link rel="stylesheet" href={STYLESHEET_URL+"/stylesheets/landing.css"}/>
-          <div className="container landingPage" >
-          </div>
+        <link rel="stylesheet" href={STYLESHEET_URL+"/stylesheets/landing.css"}/>
+        <div className="container landingPage" >
+        </div>
       </div>
       :
-       <Grid component="main">
+      <Grid component="main">
         <CssBaseline />
         <Box className="App" component="div" display="flex" flexDirection="column">
-            <span className="DashboardModeControlCaption" > Do not disturb mode </span>
-            <i className="fa fa-ban fa-2x DashboardModeControl" onClick={handleClickDoNotDisturbMode} aria-hidden="true" style={banAdditionalStyle}/>
+          <span className="DashboardModeControlCaption" > Do not disturb mode </span>
+          <i className="fa fa-ban fa-2x DashboardModeControl" onClick={handleClickDoNotDisturbMode} aria-hidden="true" style={banAdditionalStyle}/>
           <Grid container alignContent="stretch" >
             <Grid item xs={5}>
               <FilterView filterParams={filterParams} setFilterParams={setFilterParams} filters={{ search: true, places: false, price: true }} />
@@ -305,46 +299,46 @@ function TenantListingDashBoard(props) {
             <Grid className="map" item xs={7}>
 
               <div style={mapMessageContainerStyle}>
-              <section style={chatContainerStyle}>
-                <GeneralChatMainPage id="compactChattingPage" compact="true" meeting="true"/>
-              </section>
+                <section style={chatContainerStyle}>
+                  <GeneralChatMainPage id="compactChattingPage" compact="true" meeting="true"/>
+                </section>
 
-              <React.Fragment>
-                <SimpleModal show={modalShow} handle1={handleClose} caption1="close" styles={{width: '20%', height: 'auto'}}>
-                  <div style={{ marginLeft: '5px' }}> Listing Summary goes here</div>
-                </SimpleModal>
-                <div>
-                <MapContainer className='mapContainerStyle' center={center} zoom={zoom} scrollWheelZoom={true} whenCreated={setMap} >
-                  <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                  <FeatureGroup ref={groupRef}>
-                    {markers.map((marker, index) =>
-                      <DashboardMarker key={`marker-${index}`} position={marker.position}  markerIndex={index-1} icon={marker.icon} eventHandlers={{click: (e) => {onMarkerClick(e, marker.markerID)}}} >
-                        <Popup>
-                          <section style={{display: 'grid', gridTemplateColumns: '1fr 1fr', color: '#115399'}}>
-                            <section style={{marginTop: '3px'}}>
-                              <a href={(index===0)? `/listing/tenant/${currentListing._id}/get`: getChildListingUrl(index-1)} target="_blank">
-                                <i className="fas fa-external-link-alt fa-lg"></i>
-                              </a>
-                            </section>
-                            <section onClick={() => {controlMessageWindow();} } style={{color: '#a52a2a'}}>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
-                                <path d="M17 13.75l2-2V20a1 1 0 01-1 1H4a1 1 0 01-1-1V6a1 1 0 011-1h8.25l-2 2H5v12h12v-5.25zm5-8a1 1 0 01-.29.74L13.15 15 7 17l2-6.15 8.55-8.55a1 1 0 011.41 0L21.71 5a1 1 0 01.29.71zm-4.07 1.83l-1.5-1.5-6.06 6.06 1.5 1.5zm1.84-1.84l-1.5-1.5-1.18 1.17 1.5 1.5z">
-                                </path>
-                              </svg>
-                            </section>
-                          </section>
-                        </Popup>
-                        {(currentListing!==undefined) && (currentListing.coordinates!==undefined) &&
-                        <Circle 
-                          center={{lat: currentListing.coordinates.lat, lng: currentListing.coordinates.lng}}
-                          fillColor="gray" 
-                          radius={1000}/>}
-                      </DashboardMarker>
-                    )}
-                  </FeatureGroup>
-                </MapContainer>
-                </div>
-              </React.Fragment>
+                <React.Fragment>
+                  <SimpleModal show={modalShow} handle1={handleClose} caption1="close" styles={{width: '20%', height: 'auto'}}>
+                    <div style={{ marginLeft: '5px' }}> Listing Summary goes here</div>
+                  </SimpleModal>
+                  <div>
+                    <MapContainer className='mapContainerStyle' center={center} zoom={zoom} scrollWheelZoom={true} whenCreated={setMap} >
+                      <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                      <FeatureGroup ref={groupRef}>
+                        {markers.map((marker, index) =>
+                          <DashboardMarker key={`marker-${index}`} position={marker.position}  markerIndex={index-1} icon={marker.icon} eventHandlers={{click: (e) => {onMarkerClick(e, marker.markerID)}}} >
+                            <Popup>
+                              <section style={{display: 'grid', gridTemplateColumns: '1fr 1fr', color: '#115399'}}>
+                                <section style={{marginTop: '3px'}}>
+                                  <a href={(index===0)? `/listing/tenant/${currentListing._id}/get`: getChildListingUrl(index-1)} target="_blank">
+                                    <i className="fas fa-external-link-alt fa-lg"/>
+                                  </a>
+                                </section>
+                                <section onClick={() => {controlMessageWindow();} } style={{color: '#a52a2a'}}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
+                                    <path d="M17 13.75l2-2V20a1 1 0 01-1 1H4a1 1 0 01-1-1V6a1 1 0 011-1h8.25l-2 2H5v12h12v-5.25zm5-8a1 1 0 01-.29.74L13.15 15 7 17l2-6.15 8.55-8.55a1 1 0 011.41 0L21.71 5a1 1 0 01.29.71zm-4.07 1.83l-1.5-1.5-6.06 6.06 1.5 1.5zm1.84-1.84l-1.5-1.5-1.18 1.17 1.5 1.5z">
+                                    </path>
+                                  </svg>
+                                </section>
+                              </section>
+                            </Popup>
+                            {(currentListing!==undefined) && (currentListing.coordinates!==undefined) &&
+                            <Circle
+                              center={{lat: currentListing.coordinates.lat, lng: currentListing.coordinates.lng}}
+                              fillColor="gray"
+                              radius={1000}/>}
+                          </DashboardMarker>
+                        )}
+                      </FeatureGroup>
+                    </MapContainer>
+                  </div>
+                </React.Fragment>
 
               </div>
 
