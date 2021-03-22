a6c58003e9620d750eda6e4a04ee8e9e44b4aad5
Justin Seo <carrier_mania@yahoo.com>
2021-03-21 21:56:39 -0700

    issue-486: map fix

--- src/client/container/ListingPage/tenant/TenantListingDashboard.jsx
+++ src/client/container/ListingPage/tenant/TenantListingDashboard.jsx
@@ -51,9 +51,9 @@ function TenantListingDashBoard(props) {
   const { currentListing, currentChildIndex, setCurrentChildIndex, getChildListingUrl, fetchCurrentListing, mapParams, setMapParams, filterParams, setFilterParams, markerParams, setMarkerParams } = useContext(CurrentListingContext);
 
   const [modalShow, setModalShow] = useState(false);
+  const [map, setMap] = useState(null);
   const { refresh, selectedMarkerID, markers } = markerParams;
   const { search } = filterParams;
-  const [map, setMap] = useState(null);
   const { bounds, center, zoom } = mapParams;
 
   const groupRef = (node) => {
@@ -62,7 +62,7 @@ function TenantListingDashBoard(props) {
         const bounds = node.getBounds();
         if (Object.keys(bounds).length > 0) {
           map.fitBounds(bounds);
-          setMapParams({ ...mapParams, bounds: bounds });
+          setMapParams({ bounds, center: map.getCenter(), zoom: map.getZoom() });
         }
       }
     }
@@ -268,40 +268,38 @@ function TenantListingDashBoard(props) {
                   <div>
                     <MapContainer className='mapContainerStyle' center={center} zoom={zoom} scrollWheelZoom={true} whenCreated={setMap} >
                       <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                      <FeatureGroup ref={groupRef}>
-                        {
-                          markers.length > 0 ? (
-                            <>
-                              {
-                                markers.map((marker, index) =>
-                                  <DashboardMarker key={`marker-${index}`} position={marker.position}  markerIndex={index-1} icon={marker.icon} eventHandlers={{click: (e) => {onMarkerClick(e, marker.markerID)}}} >
-                                    <Popup>
-                                      <section style={{display: 'grid', gridTemplateColumns: '1fr 1fr', color: '#115399'}}>
-                                        <section style={{marginTop: '3px'}}>
-                                          <a href={(index===0)? `/listing/tenant/${currentListing._id}/get`: getChildListingUrl(index-1)} target="_blank">
-                                            <i className="fas fa-external-link-alt fa-lg"/>
-                                          </a>
-                                        </section>
-                                        <section onClick={() => {controlMessageWindow()} } style={{color: '#a52a2a'}}>
-                                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
-                                            <path d="M17 13.75l2-2V20a1 1 0 01-1 1H4a1 1 0 01-1-1V6a1 1 0 011-1h8.25l-2 2H5v12h12v-5.25zm5-8a1 1 0 01-.29.74L13.15 15 7 17l2-6.15 8.55-8.55a1 1 0 011.41 0L21.71 5a1 1 0 01.29.71zm-4.07 1.83l-1.5-1.5-6.06 6.06 1.5 1.5zm1.84-1.84l-1.5-1.5-1.18 1.17 1.5 1.5z">
-                                            </path>
-                                          </svg>
-                                        </section>
+                      {
+                        markers.length > 0 ? (
+                          <FeatureGroup ref={groupRef}>
+                            {
+                              markers.map((marker, index) =>
+                                <DashboardMarker key={`marker-${index}`} position={marker.position}  markerIndex={index-1} icon={marker.icon} eventHandlers={{click: (e) => {onMarkerClick(e, marker.markerID)}}} >
+                                  <Popup>
+                                    <section style={{display: 'grid', gridTemplateColumns: '1fr 1fr', color: '#115399'}}>
+                                      <section style={{marginTop: '3px'}}>
+                                        <a href={(index===0)? `/listing/tenant/${currentListing._id}/get`: getChildListingUrl(index-1)} target="_blank">
+                                          <i className="fas fa-external-link-alt fa-lg"/>
+                                        </a>
                                       </section>
-                                    </Popup>
-                                    {(currentListing!==undefined) && (currentListing.coordinates!==undefined) &&
-                                    <Circle
-                                      center={{lat: currentListing.coordinates.lat, lng: currentListing.coordinates.lng}}
-                                      fillColor="gray"
-                                      radius={1000}/>}
-                                  </DashboardMarker>
-                                )
-                              }
-                            </>
-                          ) : (<></>)
-                        }
-                      </FeatureGroup>
+                                      <section onClick={() => {controlMessageWindow()} } style={{color: '#a52a2a'}}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
+                                          <path d="M17 13.75l2-2V20a1 1 0 01-1 1H4a1 1 0 01-1-1V6a1 1 0 011-1h8.25l-2 2H5v12h12v-5.25zm5-8a1 1 0 01-.29.74L13.15 15 7 17l2-6.15 8.55-8.55a1 1 0 011.41 0L21.71 5a1 1 0 01.29.71zm-4.07 1.83l-1.5-1.5-6.06 6.06 1.5 1.5zm1.84-1.84l-1.5-1.5-1.18 1.17 1.5 1.5z">
+                                          </path>
+                                        </svg>
+                                      </section>
+                                    </section>
+                                  </Popup>
+                                  {(currentListing !== undefined) && (currentListing.coordinates !== undefined) &&
+                                  <Circle
+                                    center={{lat: currentListing.coordinates.lat, lng: currentListing.coordinates.lng}}
+                                    fillColor="gray"
+                                    radius={1000}/>}
+                                </DashboardMarker>
+                              )
+                            }
+                          </FeatureGroup>
+                        ) : (<></>)
+                      }
                     </MapContainer>
                   </div>
                 </React.Fragment>
