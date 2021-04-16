1e935825d5fe27932fda62a1dea7eeab9fa9a9e2
Justin Seo <carrier_mania@yahoo.com>
2021-04-16 01:48:28 -0700

    issue-486: fixed chat icon clicking on child listing

--- src/client/container/MapPage/views/ListView/ChildListing.jsx
+++ src/client/container/MapPage/views/ListView/ChildListing.jsx
@@ -3,7 +3,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './ListingComponent.css';
 import ListItem from '@material-ui/core/ListItem';
-import { Paper, Grid, Typography } from '@material-ui/core';
+import { Paper, Grid, Typography, IconButton } from '@material-ui/core';
 import Carousel from 'react-bootstrap/Carousel';
 import constructListingInformationBullets from '../../helper/helper';
 import MessageEditorIcon from '../../../../components/Message/MessageEditorIcon';
@@ -148,6 +148,13 @@ const ChildListing = React.forwardRef(({
     setModalShow(false);
   }
 
+  function handleChatIconOnClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    toggleCollapse();
+  }
+
   const scrollToBottom = () => {
     // console.log("scrollToBottom. numOfMsgHistory="+numOfMsgHistory);
     if (reference.current !== undefined && reference.current != null) {
@@ -260,7 +267,7 @@ const ChildListing = React.forwardRef(({
                 <Typography className="description__title" color="textSecondary" gutterBottom style={{fontSize: '.9rem', marginLeft: '5px', color: '#652143'}}>
                   {listingTitle}
                 </Typography>
-                <section style={{color: 'rgb(165, 42, 42)'}} onClick={toggleCollapse}>
+                <section style={{color: 'rgb(165, 42, 42)'}} onClick={(event) => {handleChatIconOnClick(event)}}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
                     <path d="M17 13.75l2-2V20a1 1 0 01-1 1H4a1 1 0 01-1-1V6a1 1 0 011-1h8.25l-2 2H5v12h12v-5.25zm5-8a1 1 0 01-.29.74L13.15 15 7 17l2-6.15 8.55-8.55a1 1 0 011.41 0L21.71 5a1 1 0 01.29.71zm-4.07 1.83l-1.5-1.5-6.06 6.06 1.5 1.5zm1.84-1.84l-1.5-1.5-1.18 1.17 1.5 1.5z">
                     </path>
